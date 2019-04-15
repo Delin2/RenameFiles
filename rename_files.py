bb166545ac4e809104d616b873c812007bc8942d
Dennis Lin <35812863+Delin2@users.noreply.github.com>
2019-04-14 20:43:30 -0400

    Removed personal info

--- rename_files.py
+++ rename_files.py
@@ -3,12 +3,12 @@ import os
 #to remove numbers from filename
 def rename_files():
     #get file names from folder
-    file_list=os.listdir(r"C:\Users\Lin-Desktop\AppData\Local\Programs\Python\Python37\Udacity\changing filenames\prank")
+    file_list=os.listdir(r"")
     #print(file_list)
     #save current directory
     saved_path = os.getcwd()
     #change directory to prank folder
-    os.chdir(r"C:\Users\Lin-Desktop\AppData\Local\Programs\Python\Python37\Udacity\changing filenames\prank")
+    os.chdir(r"")
     
     #change name of files
     for name in file_list:
